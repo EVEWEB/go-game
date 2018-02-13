d13d66264b45d9ad3c4cf6f6c0bd1fdf025ace55
EVEWEB <esme.valentine@gmail.com>
2018-02-13 11:17:41 +0000

    minor change

--- bunnymeshBackup.js
+++ bunnymeshBackup.js
@@ -8,9 +8,6 @@ var scream = new WaudSound("scream.mp3", { autoplay: false, loop: false, onload:
   screamLoaded=true;
 } });
 
-/*
-  <p> This example shows how to draw a mesh with regl </p>
-*/
 const regl = require('regl')()
 const mat4 = require('gl-mat4')
 const normals = require('angle-normals')
